41079533120f990d18658e66d32c86e2ad4fffc2
Gustavo Vasques <gvasques.dev@gmail.com>
2022-06-27 01:54:12 -0300

    Conexão com o banco de dados MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
+import {MongoClient} from "mongodb"
 
 
 const app = express();
 dotenv.config();
 
-
+let db = null;
+const mongoClient = new MongoClient(process.env.Mongo_URI);
+const promise = mongoClient.connect();
+promise.then(() => {
+    db = mongoClient.db(process.env.DATABASE);
+    console.log(chalk.green.bold("Database connection estabilished"));
+});
+promise.catch((e) => console.log("Database connection error! ", e));
 
 
 
